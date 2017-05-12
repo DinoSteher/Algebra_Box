9613358d115dc7cdf6826b982aeb1a1bc97e1c72
DinoSteher <dino.steher@fer.hr>
2017-05-12 10:26:06 +0200

    Zadaca

--- app/Http/Controllers/User/HomeController.php
+++ app/Http/Controllers/User/HomeController.php
@@ -4,9 +4,15 @@ namespace App\Http\Controllers\User;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Storage;
+use App\Models\UsersRoot;
+use Centaur\AuthManager;
+use Cartalyst\Sentinel\Users\UserInterface;
+use File;
 
 class HomeController extends Controller
 {
+	protected $sentinel;
   /**
    * Set middleware to quard controller.
    *
@@ -15,6 +21,7 @@ class HomeController extends Controller
     public function __construct()
     {
         $this->middleware('sentinel.auth');
+		$this->sentinel = app()->make('sentinel');
     }
 
     /**
@@ -24,6 +31,52 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('user.home');
+		$user = $this->sentinel->check();
+		$user_id = $user["attributes"]["id"];
+		$users_root = new UsersRoot();
+		
+		$keys = array_keys($_GET);
+		if($_GET){
+			foreach($_GET as $key=>$value){
+				if($key == 'nazivDir'){
+					$this->uploadFile($_GET['nazivDir'], $user_id);
+				}
+				if(substr($key, 0, 6)=="delete"){
+					$this->deleteFile($key);
+				}
+			}
+		}
+		$user = $this->sentinel->check();
+		$user_id = $user["attributes"]["id"];
+		$users_root = new UsersRoot();
+		$table = $users_root->get();
+		foreach($table as $key=>$file){
+			if($file["attributes"]["user_id"]== $user_id){
+				$files[] = $table[$key];
+			}
+		}
+        return view('user.home', ['files' => $files] );
     }
+	
+	public function uploadFile($name, $user_id){
+		$dirExist = Storage::disk('public')->allDirectories();	
+		if(!in_array($name, $dirExist)){
+			Storage::disk('public')->makeDirectory($name);
+			$users_root = new UsersRoot();
+			$users_root->name = $name;
+			$users_root->user_id = $user_id;
+			$users_root->save();
+		}else{
+			session()->flash('error', 'The map already exists!');
+		}
+	}
+	
+	public function deleteFile($key){
+		$name = explode("_", $key)[1];
+		$users_root = new UsersRoot();
+		$users_root->where('name', '=', $name)->delete();
+		Storage::deleteDirectory($name);
+	}
+	
+	
 }
--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -1,3 +1,4 @@
+
 @extends('layouts.index')
 
 @section('title', 'AlgebraBox | The greatest cloud storage')
@@ -8,7 +9,38 @@
     <li class="active">Home</li>
   </ol>
 </div>
-<div class="row">
+<div class="row panel-body">
+<table class="table table-hover">
+	<tr>
+		<th> Naziv dokumenta </th>
+		<th> Kreiran </th>
+		<th> Azuriran </th>
+		<th> Upravljanje </th>
+	</tr>
+    @foreach($files as $key=>$value)
+    <tr>
+        <th><a>{{$value["name"]}}</a></th>
+		<th>{{$value["created_at"]}}</th>
+		<th>{{$value["updated_at"]}}</th>
+		<td>
+			<a href="" class="btn btn-primary btn-sm" role="button">Uredi</a> 
+			<form method="GET"><button class="btn btn-danger btn-sm" id="delete_{{$value["name"]}}"       name="delete_{{$value["name"]}}">Izbrisi</button></form>
+		</td>
+    </tr>
+    @endforeach
+</table>
 
+<div class="panel-body">
+	<form method="GET">
+		<div class="form-group">
+			<label for="nazivUpload">Naziv novog direktorija</label>
+			<input type="text" name="nazivDir" id="nazivDir" class="form-control" placeholder="Upisite naziv direktorija">
+		</div>					
+		<div class="form-group">
+			<button class="btn btn-primary btn-lg" name="uploadDir" id="uploadDir">Kreiraj!</button>
+			<a class="btn btn-danger btn-lg" role="button">Odustani</a>
+		</div>
+	</form>
+</div>
 </div>
 @stop
